52c9693b2796912756e9a472159e4b1b8122c8f0
AndyMu <andymu0601@gmail.com>
2020-10-12 17:45:07 +0800

    AMBasicsPart1 完成

--- AMSwift51Practice/ViewController.swift
+++ AMSwift51Practice/ViewController.swift
@@ -13,7 +13,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        AMBasicsPart1.init()
+        AMBasicsPart1.init().SetupTest()
+        
     }
 }
 
---
+++ AMSwift51Practice/基础部分/AMBasicsPart2.swift
@@ -0,0 +1,14 @@
+//
+//  AMBasicsPart2.swift
+//  AMSwift51Practice
+//
+//  Created by AndyMu on 2020/10/12.
+//
+
+import Foundation
+
+class AMBasicsPart2: NSObject {
+    func SetupTest() {
+        
+    }
+}
