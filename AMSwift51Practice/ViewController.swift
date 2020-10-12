03dd060d2099a5d54ae8b0396003c5136b5c833b
AndyMu <andymu0601@gmail.com>
2020-10-12 16:45:21 +0800

    init

---
+++ AMSwift51Practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AMSwift51Practice
+//
+//  Created by AndyMu on 2020/10/12.
+//
+
+import UIKit
+import Foundation
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        AMBasicsPart1.init()
+    }
+}
+
---
+++ AMSwift51Practice/基础部分/AMBasicsPart1.swift
@@ -0,0 +1,15 @@
+//
+//  AMPart1.swift
+//  AMSwift51Practice
+//
+//  Created by AndyMu on 2020/10/12.
+//
+
+import Foundation
+
+class AMBasicsPart1:NSObject {
+    
+    let maximumNumberOfLoginAttempts = 10
+    var currentLoginAttempt = 0
+    
+}
