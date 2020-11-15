1ef13af622b8944437478ff7851d2d17771a3af0
AndyMu <andymu0601@gmail.com>
2020-11-15 22:16:49 +0800

    基础部分优化

--- AMSwiftGGPractice/基础部分/AMBasicsPart3.swift
+++ AMSwiftGGPractice/基础部分/AMBasicsPart3.swift
@@ -12,91 +12,123 @@ class AMBasicsPart3 {
         Test()
         Test1()
         Test2()
+        Test3()
+        Test4()
+        Test5()
     }
-    ///-----------------------------元组-----------------------------///
+    
+    // MARK: 1.11 =========================== 元组
     func Test() {
-        // 元组（tuples）把多个值组合成一个复合值。元组内的值可以是任意类型，并不要求是相同类型。
+        /// 元组（tuples）把多个值组合成一个复合值。元组内的值可以是任意类型，并不要求是相同类型。
+        /// http404Error 的类型是 (Int, String)，值是 (404, "Not Found")
         let http404Error = (404, "Not Found")
-        // http404Error 的类型是 (Int, String)，值是 (404, "Not Found")
         
-        // 你可以将一个元组的内容分解（decompose）成单独的常量和变量，然后你就可以正常使用它们了：
+        /// 你可以将一个元组的内容分解（decompose）成单独的常量和变量，然后你就可以正常使用它们了：
         let (statusCode, statusMessage) = http404Error
+        /// 输出“The status code is 404”
         print("The status code is \(statusCode)")
-        // 输出“The status code is 404”
+        /// 输出“The status message is Not Found”
         print("The status message is \(statusMessage)")
-        // 输出“The status message is Not Found”
         
-        // 如果你只需要一部分元组值，分解的时候可以把要忽略的部分用下划线（_）标记：
+        /// 如果你只需要一部分元组值，分解的时候可以把要忽略的部分用下划线（_）标记：
         let (justTheStatusCode, _) = http404Error
+        /// 输出“The status code is 404”
         print("The status code is \(justTheStatusCode)")
-        // 输出“The status code is 404”
         
-        // 你还可以通过下标来访问元组中的单个元素，下标从零开始：
+        /// 你还可以通过下标来访问元组中的单个元素，下标从零开始：
+        /// 输出“The status code is 404”
         print("The status code is \(http404Error.0)")
-        // 输出“The status code is 404”
+        /// 输出“The status message is Not Found”
         print("The status message is \(http404Error.1)")
-        // 输出“The status message is Not Found”
         
-        // 你可以在定义元组的时候给单个元素命名：
+        /// 你可以在定义元组的时候给单个元素命名：
         let http200Status = (statusCode: 200, description: "OK")
-        // 给元组中的元素命名后，你可以通过名字来获取这些元素的值：
+        /// 给元组中的元素命名后，你可以通过名字来获取这些元素的值：
+        /// 输出“The status code is 200”
         print("The status code is \(http200Status.statusCode)")
-        // 输出“The status code is 200”
+        /// 输出“The status message is OK”
         print("The status message is \(http200Status.description)")
-        // 输出“The status message is OK”
     }
-    ///-------------------可选类型 & if 语句以及强制解析 ---------------------///
+    
+    // MARK: 1.12 =========================== 可选类型
+    // FIXME:⚠️⚠️⚠️
+    /// C 和 Objective-C 中并没有可选类型这个概念。
+    /// 最接近的是 Objective-C 中的一个特性，一个方法要不返回一个对象要不返回 nil，nil 表示“缺少一个合法的对象”。
+    /// 然而，这只对对象起作用——对于结构体，基本的 C 类型或者枚举类型不起作用。
+    /// 对于这些类型，Objective-C 方法一般会返回一个特殊值（比如 NSNotFound）来暗示值缺失。
+    /// 这种方法假设方法的调用者知道并记得对特殊值进行判断。
+    /// 然而，Swift 的可选类型可以让你暗示任意类型的值缺失，并不需要一个特殊值。
+    
     func Test1() {
         let possibleNumber = "123"
         let convertedNumber = Int(possibleNumber)
-        // convertedNumber 被推测为类型 "Int?"， 或者类型 "optional Int"
+        /// convertedNumber 被推测为类型 "Int?"， 或者类型 "optional Int"
         print(convertedNumber!)
-        
-        // 你可以给可选变量赋值为 nil 来表示它没有值：
+    }
+    
+    // MARK: 1.12.1 =========================== nil
+    func Test2() {
+        /// 你可以给可选变量赋值为 nil 来表示它没有值：
         var serverResponseCode: Int? = 404
-        // serverResponseCode 包含一个可选的 Int 值 404
+        /// serverResponseCode 包含一个可选的 Int 值 404
         serverResponseCode = nil
-        // serverResponseCode 现在不包含值
+        /// serverResponseCode 现在不包含值
         print(serverResponseCode ?? "")
         
-        /* nil 不能用于非可选的常量和变量。
-        如果你的代码中有常量或者变量需要处理值缺失的情况，请把它们声明成对应的可选类型。*/
+        /// nil 不能用于非可选的常量和变量，如果你的代码中有常量或者变量需要处理值缺失的情况，
+        /// 请把它们声明成对应的可选类型
         
-        /* Swift 的 nil 和 Objective-C 中的 nil 并不一样。
-        在 Objective-C 中，nil 是一个指向不存在对象的指针。
-        在 Swift 中，nil 不是指针——它是一个确定的值，用来表示值缺失。
-        任何类型的可选状态都可以被设置为 nil，不只是对象类型。*/
-        
-        // 如果可选类型有值，它将不等于 nil：
+        /// _ 被自动设置为 nil
+        var _: String?
+    }
+    // FIXME:⚠️⚠️⚠️
+    /// Swift 的 nil 和 Objective-C 中的 nil 并不一样。
+    /// 在 Objective-C 中，nil 是一个指向不存在对象的指针。
+    /// 在 Swift 中，nil 不是指针——它是一个确定的值，用来表示值缺失。
+    /// 任何类型的可选状态都可以被设置为 nil，不只是对象类型。
+    
+    // MARK: 1.12.2 =========================== if 语句以及强制解析
+    func Test3() {
+        let possibleNumber = "123"
+        let convertedNumber = Int(possibleNumber)
+        /// 如果可选类型有值，它将不等于 nil：
         if convertedNumber != nil {
             print("convertedNumber contains some integer value.")
         }
-        /* 当你确定可选类型确实包含值之后，你可以在可选的名字后面加一个感叹号（!）来获取值。
-         这个惊叹号表示“我知道这个可选有值，请使用它。”这被称为可选值的强制解析（forced unwrapping）：*/
+        /// 当你确定可选类型确实包含值之后，你可以在可选的名字后面加一个感叹号（!）来获取值。
+        /// 这个惊叹号表示“我知道这个可选有值，请使用它。”这被称为可选值的强制解析（forced unwrapping）：*/
         if convertedNumber != nil {
+            /// 输出“convertedNumber has an integer value of 123.”
             print("convertedNumber has an integer value of \(convertedNumber!).")
         }
-        // 输出“convertedNumber has an integer value of 123.”
     }
     
-    ///------------------------可选绑定------------------------------///
-    func Test2() {
+    // MARK: 1.12.3 =========================== 可选绑定
+    
+    /// 使用可选绑定（optional binding）来判断可选类型是否包含值，如果包含就把值赋给一个临时常量或者变量。
+    /// 可选绑定可以用在 if 和 while语句中，这条语句不仅可以用来判断可选类型中是否有值，
+    /// 同时可以将可选类型中的值赋给一个常量或者变量。
+    /// if 和 while 语句，请参考 控制流。
+    
+    func Test4() {
         let possibleNumber = "100"
-        
+        /// 输出“'123' has an integer value of 123”
         if let actualNumber = Int(possibleNumber) {
             print("\'\(possibleNumber)\' has an integer value of \(actualNumber)")
         } else {
             print("\'\(possibleNumber)\' could not be converted to an integer")
         }
-        // 输出“'123' has an integer value of 123”
         
-        /* 你可以包含多个可选绑定或多个布尔条件在一个 if 语句中，只要使用逗号分开就行。
-         只要有任意一个可选绑定的值为 nil，或者任意一个布尔条件为 false，则整个 if 条件判断为 false。*/
-        if let firstNumber = Int("4"), let secondNumber = Int("42"), firstNumber < secondNumber && secondNumber < 100 {
+        /// 你可以包含多个可选绑定或多个布尔条件在一个 if 语句中，只要使用逗号分开就行。
+        /// 只要有任意一个可选绑定的值为 nil，或者任意一个布尔条件为 false，则整个 if 条件判断为 false。
+        /// 输出“4 < 42 < 100”
+        if let firstNumber = Int("4"),
+           let secondNumber = Int("42"),
+           firstNumber < secondNumber && secondNumber < 100 {
             print("\(firstNumber) < \(secondNumber) < 100")
         }
-        // 输出“4 < 42 < 100”
-
+        
+        /// 输出“4 < 42 < 100”
         if let firstNumber = Int("4") {
             if let secondNumber = Int("42") {
                 if firstNumber < secondNumber && secondNumber < 100 {
@@ -104,7 +136,27 @@ class AMBasicsPart3 {
                 }
             }
         }
-        // 输出“4 < 42 < 100”
+    }
+    
+    // MARK: 1.12.4 =========================== 隐式解析可选类型
+    func Test5() {
+        /// 普通可选类型
+        let possibleString: String? = "An optional string."
+        let forcedString: String = possibleString! // 需要感叹号来获取值
+        print(forcedString)
+        
+        ///隐式解析可选类型例子
+        let assumedString: String! = "An implicitly unwrapped optional string."
+        let implicitString: String = assumedString  // 不需要感叹号
+        print(implicitString)
+        
+        
+        /// 如果你在隐式解析可选类型没有值的时候尝试取值，会触发运行时错误。
+        /// 和你在没有值的普通可选类型后面加一个惊叹号一样。
+        
+        //FIXME:⚠️⚠️⚠️
+        /// 如果一个变量之后可能变成 nil 的话请不要使用隐式解析可选类型。
+        /// 如果你需要在变量的生命周期中判断是否是 nil 的话，请使用普通可选类型。
     }
     
 }
--- AMSwiftGGPractice/基础部分/AMBasicsPart4.swift
+++ AMSwiftGGPractice/基础部分/AMBasicsPart4.swift
@@ -9,41 +9,49 @@ import Foundation
 
 class AMBasicsPart4 {
     func SetupTest() {
-        Test()
         Test1()
     }
-    ///--------------------------隐式解析可选类型-----------------------------///
-    func Test() {
-        let possibleString: String? = "An optional string."
-        let forcedString: String = possibleString! // 需要感叹号来获取值
-        print(forcedString)
-        let assumedString: String! = "An implicitly unwrapped optional string."
-        let implicitString: String = assumedString  // 不需要感叹号
-        print(implicitString)
-        /* 如果你在隐式解析可选类型没有值的时候尝试取值，会触发运行时错误。
-         和你在没有值的普通可选类型后面加一个惊叹号一样。*/
-        
-        
-        /* 如果一个变量之后可能变成 nil 的话请不要使用隐式解析可选类型。
-         如果你需要在变量的生命周期中判断是否是 nil 的话，请使用普通可选类型。*/
-    }
-    ///--------------------------断言和先决条件-----------------------------///
-    /*断言和先决条件是在运行时所做的检查。你可以用他们来检查在执行后续代码之前是否一个必要的条件已经被满足了。
-     如果断言或者先决条件中的布尔条件评估的结果为 true（真），则代码像往常一样继续执行。
-     如果布尔条件评估结果为 false（假），程序的当前状态是无效的，则代码执行结束，应用程序中止。*/
     
-    /*断言和先决条件的不同点是，他们什么时候进行状态检测：
-     断言仅在调试环境运行，而先决条件则在调试环境和生产环境中运行。
-     在生产环境中，断言的条件将不会进行评估。
-     这个意味着你可以使用很多断言在你的开发阶段，但是这些断言在生产环境中不会产生任何影响。*/
+    // MARK: 1.13 =========================== 错误处理
+    
+    /// 你可以使用 错误处理（error handling） 来应对程序执行中可能会遇到的错误条件。
+    /// 相对于可选中运用值的存在与缺失来表达函数的成功与失败，错误处理可以推断失败的原因，并传播至程序的其他部分。
+    
+    func makeASandwich() throws {
+        /// ...
+    }
+
     func Test1() {
+        do {
+            try makeASandwich()
+            /// 没有错误消息抛出
+        } catch {
+            /// 有一个错误消息抛出
+        }
+    }
+    
+    // MARK: 1.14 =========================== 断言和先决条件
+    
+    /// 断言和先决条件是在运行时所做的检查。你可以用他们来检查在执行后续代码之前是否一个必要的条件已经被满足了。
+    /// 如果断言或者先决条件中的布尔条件评估的结果为 true（真），则代码像往常一样继续执行。
+    /// 如果布尔条件评估结果为 false（假），程序的当前状态是无效的，则代码执行结束，应用程序中止。
+    
+    /// 断言和先决条件的不同点是，他们什么时候进行状态检测：
+    /// 断言仅在调试环境运行，而先决条件则在调试环境和生产环境中运行。
+    /// 在生产环境中，断言的条件将不会进行评估。
+    /// 这个意味着你可以使用很多断言在你的开发阶段，但是这些断言在生产环境中不会产生任何影响。
+    
+    // MARK: 1.14.1 =========================== 使用断言进行调试
+    
+    func Test2() {
         let age = 3
-        /* Swift 标准库的 assert(_:_:file:line:) 函数来写一个断言。
-         向这个函数传入一个结果为 true 或者 false 的表达式以及一条信息，当表达式的结果为 false 的时候这条信息会被显示：*/
-        assert(age >= 0, "A person's age cannot be less than zero")
-        // 因为 age < 0，所以断言会触发
+        /// Swift 标准库的 assert(_:_:file:line:) 函数来写一个断言。
+        /// 向这个函数传入一个结果为 true 或者 false 的表达式以及一条信息，当表达式的结果为 false 的时候这条信息会被显示
         
-        // 如果代码已经检查了条件，你可以使用 assertionFailure(_:file:line:) 函数来表明断言失败了
+        /// 因为 age < 0，所以断言会触发
+        assert(age >= 0, "A person's age cannot be less than zero")
+        
+        /// 如果代码已经检查了条件，你可以使用 assertionFailure(_:file:line:) 函数来表明断言失败了
         if age > 10 {
             print("You can ride the roller-coaster or the ferris wheel.")
         } else if age > 0 {
@@ -51,30 +59,18 @@ class AMBasicsPart4 {
         } else {
             assertionFailure("A person's age can't be less than zero.")
         }
-        
+    }
+
+    // MARK: 1.14.2 =========================== 强制执行先决条件
+    
+    func Test3() {
         let index = 10
         
-        /* 你可以使用全局 precondition(_:_:file:line:) 函数来写一个先决条件。向这个函数传入一个结果为 true 或者 false 的表达式以及一条信息，当表达式的结果为 false 的时候这条信息会被显示：*/
-        // 在一个下标的实现里...
+        /// 你可以使用全局 precondition(_:_:file:line:) 函数来写一个先决条件。
+        /// 向这个函数传入一个结果为 true 或者 false 的表达式以及一条信息，当表达式的结果为 false 的时候这条信息会被显示：
+    
+        /// 在一个下标的实现里...
         precondition(index > 0, "Index must be greater than zero.")
     }
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
     
 }
